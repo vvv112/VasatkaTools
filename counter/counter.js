a5f262eb4a34fd27bc7903a478cda66a9bf11d0f
memerenok <65280795+vvv112@users.noreply.github.com>
2023-10-31 11:00:22 +0100

    Update counter.js

--- counter/counter.js
+++ counter/counter.js
@@ -1,14 +1,11 @@
 
-alert(document.getElementById("numbox"))
 var num = 0
 function plus() {
 num++
-alert(num)
 document.getElementById("numbox").value = num
 };
 
 function minus() {
 num--
-alert(num)
 document.getElementById("numbox").value = num
 };
